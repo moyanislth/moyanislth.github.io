d97f27278b25e5c4a2c93c7d9954a32670c85d21
unknown <3094673523@qq.com>
2023-10-28 18:25:14 +0800

    Site updated: 2023-10-28 18:25:14

--- js/imgloaded.js
+++ js/imgloaded.js
@@ -1,20 +1,23 @@
+// 首页头图加载优化
 /**
  * @description 实现medium的渐进加载背景的效果
  */
-(function() {
   // 定义ProgressiveLoad类
   class ProgressiveLoad {
     constructor(smallSrc, largeSrc) {
       this.smallSrc = smallSrc;
       this.largeSrc = largeSrc;
+      this.initScrollListener(),
       this.initTpl();
-      //监听动画事件结束
-      this.container.addEventListener('animationend', () => {
-        //隐藏小图
-        this.smallStage.style.display = 'none'; 
-      }, {once: true});
     }
-
+    // 这里的1是滚动全程渐变 改为0.3就是前30%渐变后固定前30%产生的渐变效果
+    initScrollListener() {
+      window.addEventListener("scroll", (()=>{
+        var e = Math.min(window.scrollY / window.innerHeight, 1);
+        this.container.style.setProperty("--process", e)
+      }
+      ))
+    }
     /**
      * @description 生成ui模板
      */
@@ -22,17 +25,21 @@
       this.container = document.createElement('div');
       this.smallStage = document.createElement('div');
       this.largeStage = document.createElement('div');
+      this.video = document.createElement('div');
       this.smallImg = new Image();
       this.largeImg = new Image();
       this.container.className = 'pl-container';
+      this.container.style.setProperty("--process", 0),
       this.smallStage.className = 'pl-img pl-blur';
       this.largeStage.className = 'pl-img';
+      this.video.className = 'pl-video';
       this.container.appendChild(this.smallStage);
       this.container.appendChild(this.largeStage);
+      this.container.appendChild(this.video);
       this.smallImg.onload = this._onSmallLoaded.bind(this);
       this.largeImg.onload = this._onLargeLoaded.bind(this);
     }
-
+  
     /**
      * @description 加载背景
      */
@@ -40,7 +47,6 @@
       this.smallImg.src = this.smallSrc;
       this.largeImg.src = this.largeSrc;
     }
-
     /**
      * @description 大图加载完成
      */
@@ -48,8 +54,7 @@
       this.largeStage.classList.add('pl-visible');
       this.largeStage.style.backgroundImage = `url('${this.largeSrc}')`;
     }
-
-    /**
+     /**
      * @description 小图加载完成
      */
     _onSmallLoaded() {
@@ -57,7 +62,7 @@
       this.smallStage.style.backgroundImage = `url('${this.smallSrc}')`;
     }
   }
-
+  
   const executeLoad = (config, target) => {
     console.log('执行渐进背景替换');
     const isMobile = window.matchMedia('(max-width: 767px)').matches;
@@ -71,7 +76,7 @@
     }
     loader.progressiveLoad();
   };
-
+  
   const config = {
     smallSrc: '/img/xiaotu.jpg', // 小图链接 尽可能配置小于100k的图片
     largeSrc: '/img/tu.jpg', // 大图链接 最终显示的图片
@@ -79,32 +84,30 @@
     mobileLargeSrc: '/img/sjdt.jpg', // 手机端大图链接 最终显示的图片
     enableRoutes: ['/'],
     };
-
+  
   function initProgressiveLoad(config) {
     // 每次加载前先清除已有的元素
-    const container = document.querySelector('.pl-container');
+    const container = document.querySelector('.pl-container'); 
     if (container) {
-      container.remove();
+      container.remove(); 
     }
     const target = document.getElementById('page-header');
     if (target && target.classList.contains('full_page')) {
       executeLoad(config, target);
     }
   }
-
+  
   function onPJAXComplete(config) {
     const target = document.getElementById('page-header');
     if (target && target.classList.contains('full_page')) {
       initProgressiveLoad(config);
     }
   }
-
+  
   document.addEventListener("DOMContentLoaded", function() {
     initProgressiveLoad(config);
   });
-
+  
   document.addEventListener("pjax:complete", function() {
     onPJAXComplete(config);
   });
-
-})();
